a7edb86b8d713382bb50716d47f976c91579e725
aoipoteto <tani.aoi3434@gmail.com>
2023-12-26 21:44:28 +0900

    hand修正

--- hand.rb
+++ hand.rb
@@ -16,8 +16,12 @@ class Hand
             aces += 1 if card.value == 'A' #カードがAの場合Aの数を１増やす
             sum + card.point #現在の合計ポイントにカードのポイントを加算
         end
+
+        aces.times { total -= 10 if total > 21 }
+        total
     end
 
     def busted?
+        points > 21
     end
 end
